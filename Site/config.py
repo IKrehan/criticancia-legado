336179455f0f9783082f786b398b25f1e8e4c4aa
IKrehan <irvigkrehan@hotmail.com>
2020-02-04 04:17:45 -0300

    Criticância

--- Site/config.py
+++ Site/config.py
@@ -1,15 +1,11 @@
 import os
 
 db_path = os.path.abspath('Site/db.sqlite3').replace('\\', '/')
-up_path = os.path.abspath('upload.txt').replace('\\', '/')
 
 SECRET_KEY = '''a+Wt@Y/CbD_zSHF=.:T[~uM>"Cc5q-DXu6aL;sY@j;W953S99+P?*L}
 xb<Y(jEQ8{q=HDC3*#yRpa#KUduHFF5UDz7[F+f'`W`zfFS]{nf>dkp7Ew.y9V2nnhh`r)_?'''
 
 SQLALCHEMY_DATABASE_URI = f"sqlite:////{db_path}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-UPLOAD_FOLDER = 'Site\static'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp3'}
 
 #print(db_path)
